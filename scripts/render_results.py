bfd911d186478aad8615c23e6815d01e2e99f3cf
sinapy <sinasarsharpour@cmail.carleton.ca>
2024-11-13 02:57:20 -0500

    feat: Optimize

--- scripts/render_results.py
+++ scripts/render_results.py
@@ -18,6 +18,8 @@ class Render360:
             self.device = "mps"
         else:
             self.device = "cpu"
+            
+        self.pose_id = pose_id
 
         #   creates a SMPL instance, and samples it T pose
         smpl_path = os.path.abspath(os.path.join(__file__ ,"../../sample-data/SMPL/models/"))
@@ -42,21 +44,33 @@ class Render360:
 
     def adjust_body_pose(self, pose_id):
         if pose_id == 1:
-            self.body_pose[0, 47] = -1.35  # Adjusts arm angle
-            self.body_pose[0, 50] =  1.30  # Adjusts opposite arm angle
-            self.body_pose[0, 4] =  0.10   # Slightly bends knees for a relaxed stance
-            self.body_pose[0, 5] = -0.10   # Balances the knee angle
-            self.body_pose[0, 23] = 0.05   # Slightly twists the torso for balance
-            self.body_pose[0, 26] = -0.05  # Counter-twist for the opposite side
-            self.body_pose[0, 35] = -0.10  # Slightly bends elbows
-            self.body_pose[0, 44] =  0.05
-            self.body_pose[0, 47] = -0.05  # (1, F, 3)
+            self.body_pose[0, 0] = -2  # LEFT LEG Back_Forward : Moves the leg back or forward, positive for back and negative for forward
+            self.body_pose[0, 3] = -1.6 # RIGHT LEG Back_Forward: Moves the leg back or forward, postiive for back and negative for forward
+            self.body_pose[0, 9] = 0.3 # LEFT LEG X axis knee bend: positive for back, and negative for forward
+            self.body_pose[0, 11] = -1.5 # LEFT LEG Z axis knee rotation sideways: positive for outward, negative for inward
+            self.body_pose[0, 12] = 1.6 # RIGHT LEG X axis knee bend: positive for back and negative for forward (negative is not recommended)
+            self.body_pose[0, 18] = 0.9 # LEFT ANKLE X axis (do ballet move or the move you do to look taller): positive for down, negative for up
+            self.body_pose[0, 21] = -0.1 # RIGHT ANKLE X axis (do ballet move): positive for down, and negative for up
+            self.body_pose[0, 45] = -0.5 # LEFT SHOULDER JOINT WITHOUT SHOLDER ROTATE X axis: positive for inward, negative for outward
+            self.body_pose[0, 47] = -0.7 # LEFT SHOULDER JOINT WITHOUT SHOULDER ROTATE Z axis up/ down: positive for up and negative for down
+            self.body_pose[0, 48] = -1.2 # RIGHT SHOULDER JOINT WITHOUT SHOULDER (FOREARM?) ROTATE X axis inward /outward: positive for inward, negative for outward
+            self.body_pose[0, 50] = 1.4 # RIGHT SHOULDER JOINT WITHOUT SHOULDER up / down Rotate Z axis : positive for down, negative for up
+            self.body_pose[0, 53] = -1.7 # LEFT HAND FORARM ROTATE Z axis up down : positive for up and negative for down
+            self.body_pose[0, 54] = 1.2 # RIGHT FOREARM ROTATE X axis : positive for inward, negative for outward
+            self.body_pose[0, 55] = 0.1 # RIGHT FOREARM ROTATE Y axis back / forward : positive for inward (forward), negative for outward (backward)
+            self.body_pose[0, 56] = 0.1 # RIGH FORARM ROTATE Z axis up / down : positive for down and negative for up
         elif pose_id == 2:
-            self.body_pose[0, 47] = -1.35
-            self.body_pose[0, 50] =  1.30
+            self.body_pose[0, 47] = -1.5 # LEFT SHOULDER JOINT WITHOUT SHOULDER ROTATE Z axis up/ down: positive for up and negative for down
+            self.body_pose[0, 50] = 1.5 # RIGHT SHOULDER JOINT WITHOUT SHOULDER up / down Rotate Z axis : positive for down, negative for up
         elif pose_id == 3:
-            self.body_pose[0, 47] = -1.35
-            self.body_pose[0, 50] =  1.30
+            self.body_pose[0, 0] = 0.2  # LEFT LEG Back_Forward : Moves the leg back or forward, positive for back and negative for forward
+            self.body_pose[0, 47] = -0.70 # LEFT SHOULDER JOINT WITHOUT SHOULDER ROTATE Z axis up/ down: positive for up and negative for down
+            self.body_pose[0, 50] = 1.5 # RIGHT SHOULDER JOINT WITHOUT SHOULDER up / down Rotate Z axis : positive for down, negative for up
+            self.body_pose[0, 53] = -1.5 # LEFT HAND FORARM ROTATE Z axis up down : positive for up and negative for down
+            self.body_pose[0, 57] = 1.5 # LEFT PALM ROTATE X axis : positive for inward and negative for outward
+            self.body_pose[0, 58] = -1 # LEFT PALM ROTATE Y axis : positive for back, negative for forward
+            self.body_pose[0, 59] = 0 # LEFT PALM ROTATE Z axis : positive for up and negative for down
+            self.body_pose[0, 45] = -0.3 # LEFT SHOULDER JOINT WITHOUT SHOLDER ROTATE X axis: positive for inward, negative for outward
 
     def render_textures(self, textures_folder, output_dir):
 
@@ -78,7 +92,7 @@ class Render360:
                 render_360_gif(self.device, self.verts,
                         current_image_np, self.verts_uvs,
                         self.faces_uvs, self.faces_verts.verts_idx,
-                        output_texture_path.replace(".png", "-360.gif"))
+                        output_texture_path.replace(".png", f"-POSEID-{self.pose_id}-360.gif"))
                 break
 
 parser = argparse.ArgumentParser(description= 'Renders SMPL 360 gifs given input textures')
